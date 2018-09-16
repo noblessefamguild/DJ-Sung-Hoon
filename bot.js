23dec0c9d7f14b76607b6fdcaa01e6dcdf0dc1f5
noblessefamguild <43304176+noblessefamguild@users.noreply.github.com>
2018-09-16 10:30:04 +0700

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,8 @@
+const Discord = require ('discord.js')
+const ytdl = require ('ytdl-core')
+const bot = new Discord.Client()
+
+bot.login('NDkwMTU4MjU5NDcxMjUzNTE2.Dn8VDQ.m4iC8nstNiLSDxXxYKqMmoPnqFM')
+bot.on("ready", () => {
+    console.log('Switch On !')
+})
